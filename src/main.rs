e241f346d43f5921c7e57a46f528a11826a02028
Steven Black <steveb@stevenblack.com>
2020-03-26 01:21:25 -0400

    First pass: read the file, and dump its contents.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+// ---
+
 fn main() {
-    println!("Hello, world!");
+    let hostsfile = "/Users/Steve/Dropbox/dev/hosts/hosts";
+    let lines = lines_from_file(hostsfile);
+                           
+    for line in lines {
+        println!("{:?}", line);
+    }
 }
