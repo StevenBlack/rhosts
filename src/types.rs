3f96523e4548ba504d4d73cf11345346100c9694
Steven Black <steveb@stevenblack.com>
2022-01-21 00:35:46 -0500

    Whoops forgot to add this new types package.

---
+++ src/types.rs
@@ -0,0 +1,89 @@
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{prelude::*, BufReader},
+};
+
+pub type Domain = String;
+pub struct Host {
+    ip: String,
+    domain: Domain,
+}
+
+pub type Hosts = Vec<Host>;
+
+#[derive(Debug, Default)]
+pub struct Hostssource {
+    pub location: String,
+    pub raw_list: Vec<String>,
+    pub list_header:  Vec<String>,
+	pub domains: Vec<Domain>,
+	pub tlds: HashMap<String, i32>,
+	pub tldtallies: Vec<i32>,
+	pub duplicates: Vec<Domain>
+}
+
+// impl HostsMethods for Hostssource {
+impl Hostssource {
+    pub fn load(&mut self, src: &str) {
+        self.location = src.to_string();
+        let file = File::open(src).expect("no such file");
+        let buf = BufReader::new(file);
+        self.raw_list = buf.lines()
+            .map(|l| l.expect("Could not parse line"))
+            .collect();
+        self.normalize();
+    }
+
+    fn normalize(&mut self) {
+        self.trimlines();
+        self.saveheader();
+        self.removeblanklines();
+        self.removecommentlines();
+    }
+
+    fn trimlines(&mut self) {
+        let mut lines: Vec<String> = self.raw_list.clone();
+        lines
+        .iter_mut()
+        .for_each(|line| *line = line
+            .trim()
+            .to_string()
+        );
+        self.domains = lines.clone();
+    }
+
+    fn removeblanklines(&mut self) {
+        let mut lines: Vec<String> = self.domains.clone();
+        lines.retain(|line | line.chars().count() > 0);
+        self.domains = lines;
+    }
+
+    fn removecommentlines(&mut self) {
+        let mut lines: Vec<String> = self.domains.clone();
+        lines.retain(|line | !line.starts_with("#"));
+        self.domains = lines;
+    }
+
+    fn saveheader(&mut self) {
+        for x in 0..self.raw_list.len() {
+            let line = self.raw_list[x].clone();
+            if line.starts_with("#") {
+              self.list_header.push(line);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_load() {
+    // assert_eq!(2 + 2, 4);
+    let mut s = Hostssource{
+        ..Default::default()
+    };
+    s.load("/Users/Steve/Dropbox/dev/hosts/hosts");
+    assert_eq!(s.location, "/Users/Steve/Dropbox/dev/hosts/hosts");
+    assert!(s.list_header.len() > 0);
+    assert!(s.raw_list.len() > 0);
+    assert!(s.domains.len() > 0);
+}
