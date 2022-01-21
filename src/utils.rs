14b97dc232097d67facc98482d43e1131417be2e
Steven Black <steveb@stevenblack.com>
2022-01-21 18:02:49 -0500

    Now handle tabs as well as spaces when normalizing.

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,6 @@
+use regex::Regex;
+
+
 // A place for utility functions
 pub fn sep(n: usize) {
     println!("{}", "-".repeat(n));
@@ -9,10 +12,20 @@ pub fn print_type_of<T>(_: &T) {
 }
 
 pub fn vtrim(v: &mut Vec<String>) -> &mut Vec<String> {
-    v.iter_mut().for_each(|line| *line = line.trim().to_string());
+    v.iter_mut()
+    .for_each(
+        |line| {
+            *line = one_space(line.trim())
+        }
+    );
     v
 }
 
+fn one_space(s: &str) -> String {
+    let re_tabs = Regex::new(r"\t+").unwrap();
+    let re_space = Regex::new(r"\s+").unwrap();
+    re_space.replace_all(&re_tabs.replace_all(s, " "), " ").to_string()
+}
 
 #[test]
 fn test_vtrim() {
@@ -21,10 +34,14 @@ fn test_vtrim() {
     v.push("      Line 1".to_string());
     v.push("   Line 2   ".to_string());
     v.push("Line 3   ".to_string());
+    // embedded spaces and tabs
+    v.push("  127.0.0.1	10iski.com   ".to_string());
+
     let foo = vtrim(&mut v);
     assert_eq!(foo[0], "Line 1".to_string());
     assert_eq!(foo[1], "Line 2".to_string());
     assert_eq!(foo[2], "Line 3".to_string());
+    assert_eq!(foo[3], "127.0.0.1 10iski.com".to_string());
 }
 
 pub fn stripblanklines(v: &mut Vec<String>) -> &mut Vec<String> {
