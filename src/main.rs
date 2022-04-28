d30ebf69b6ff2f95810c825a14257e88fb7b8f88
Steven Black <steveb@stevenblack.com>
2022-04-27 23:12:38 -0400

    Evolve the args struct for clap.

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,105 @@ use clap::{AppSettings, Arg, ArgMatches, Command, Parser};
 use std::env;
 use std::io::Write;
 
+use config::get_shortcuts;
+
 mod config;
 mod cmd;
 mod types;
 mod utils;
 
+#[derive(Debug, Default, Parser)]
+#[clap(author, version, about, long_about = None)]
+#[deny(missing_docs)]
+
+/// All the app settings
+pub struct Args {
+    /// The main hosts file, the basis for comparison.
+    #[clap(short, long="main")]
+    mainhosts: String,
+
+    /// The hosts file to compare to mainhosts.
+    #[clap(short, long="compare")]
+    comparehosts: Option<String>,
+
+    /// The ip address to use for hosts
+    #[clap(long="ip", default_value="0.0.0.0")]
+    iplocalhost: String,
+
+    /// Add default hosts assigments
+    #[clap(short='d', long="default_hosts")]
+    adddefaults: Option<bool>,
+
+    /// Sort the domains.
+    #[clap(short='s', long="sort")]
+    alpha_sort: Option<bool>,
+
+    /// Print the domains to std out.
+    #[clap(short, long)]
+    output: Option<bool>,
+
+    /// Domains with no IP addresses.
+    #[clap(short='p', long="plain")]
+    plain_output: Option<bool>,
+
+    /// Print the domains to std out.
+    #[clap(long)]
+    stats: Option<bool>,
+
+    /// Print the intersection of lists.
+    #[clap(long)]
+    intersection_list: Option<bool>,
+
+    /// Print top level domain tallies.
+    #[clap(long)]
+    tld: Option<bool>,
+
+    /// Omit the file comment headers in output.
+    #[clap(long)]
+    noheader: Option<bool>,
+
+    /// Use the contents of the system clipboard as compare hosts.
+    #[clap(long="clip")]
+    sysclipboard: Option<bool>,
+
+    /// List the unique domain names
+    #[clap(short, long="unique")]
+    uniquelist: Option<bool>,
+
+    root: Option<bool>,
+}
+
+impl Args {
+    pub fn new() -> Args {
+        // Special code goes here ...
+        let mut shortcuts = get_shortcuts();
+        let mut d = Args {
+          mainhosts: shortcuts.get("base").unwrap().to_owned(),
+          iplocalhost: "0.0.0.0".to_string(),
+          stats: Some(true),
+          ..Default::default()
+        };
+        d
+    }
+}
+
+#[test]
+fn test_args() {
+    let d = Args::new();
+    assert_eq!(d.mainhosts, get_shortcuts().get("base").unwrap().to_owned());
+    assert_eq!(d.comparehosts, None);
+    assert_eq!(d.iplocalhost, "0.0.0.0".to_string());
+    assert_eq!(d.tld, None);
+    assert_eq!(d.stats, Some(true));
+}
+
 fn main() {
+
+    // let args = Args::parse();
+
     let app = create_clap_app();
+
+println!("{:?}", create_clap_app().get_matches());
 
     // Check which subcomamnd the user ran...
     let res = match app.get_matches().subcommand() {
@@ -48,7 +140,8 @@ fn create_clap_app() -> Command<'static> {
         )
         .subcommand(cmd::init::make_subcommand())
         .subcommand(cmd::build::make_subcommand())
-        .subcommand(cmd::clean::make_subcommand());
+        .subcommand(cmd::clean::make_subcommand())
+        .subcommand(cmd::core::make_subcommand());
 
     // Base (main) hosts file
     let main_option = Arg::new("main")
