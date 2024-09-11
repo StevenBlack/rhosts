b3d4143248ca533745354f6bd96494c46a1a48be
Steven Black <steveb@stevenblack.com>
2024-09-11 14:53:40 -0400

    Cleanups.

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -1,6 +1,4 @@
-use std::collections::HashSet;
-
-use crate::{config::{get_products_json, get_sources_by_tag, Components}, types::Amalgam, Arguments};
+use crate::{types::Amalgam, Arguments};
 use anyhow::Error;
 
 // Build command implementation
@@ -20,45 +18,6 @@ pub async fn execute(args: Arguments) -> Result<(), Error> {
 }
 
 pub async fn buildproduct(name: String)  {
-    // let json = get_products_json();
-    // let products: Components = serde_json::from_str(json.as_str()).expect("Invalid JSON in recipe.");
-
-    // let mut iter = products.iter();
-    // let product_spec = iter.find(|x| x.name == name);
-    // if product_spec.is_none() {
-    //     return ();
-    // }
-    // let spec = product_spec.unwrap();
-    // let tags = spec.tags.clone();
-    // let mut hs: HashSet<String> = HashSet::new();
-    // for tag in tags {
-    //     let sources = get_sources_by_tag(tag);
-    //     for s in sources.clone() {
-    //         hs.insert(s.url);
-    //     }
-    // }
     let amalgam = Amalgam::new(vec!(name)).await;
     println!("{:?}", amalgam.domains.len());
 }
-
-// pub async fn buildproduct(name: String)  {
-//     let json = get_products_json();
-//     let products: Components = serde_json::from_str(json.as_str()).expect("Invalid JSON in recipe.");
-
-//     let mut iter = products.iter();
-//     let product_spec = iter.find(|x| x.name == name);
-//     if product_spec.is_none() {
-//         return ();
-//     }
-//     let spec = product_spec.unwrap();
-//     let tags = spec.tags.clone();
-//     let mut hs: HashSet<String> = HashSet::new();
-//     for tag in tags {
-//         let sources = get_sources_by_tag(tag);
-//         for s in sources.clone() {
-//             hs.insert(s.url);
-//         }
-//     }
-//     let amalgam = Amalgam::new(Vec::from_iter(hs)).await;
-//     println!("{:?}", amalgam.domains.len());
-// }
