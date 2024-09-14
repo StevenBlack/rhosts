9aceb7b3387975cc644d0dd613274ed74ffb107d
Steven Black <steveb@stevenblack.com>
2024-09-14 10:39:35 -0400

    Initial hacking with the build command.

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -2,23 +2,24 @@ use crate::{types::Amalgam, Arguments};
 use anyhow::Error;
 
 // Build command implementation
+// This is all very experimental right now.
 pub async fn execute(args: Arguments) -> Result<(), Error> {
     if args.verbose {
         println!("Handled by 'build'.");
     }
 
     // for now, let's just build the base list
-    // buildproduct("base".to_string()).await;
+    buildproduct("base".to_string()).await;
     // buildproduct("s-only".to_string()).await;
     // buildproduct("p".to_string()).await;
     // buildproduct("p-only".to_string()).await;
     // buildproduct("xyz".to_string()).await;
-    buildproduct("fgps".to_string()).await;
+    // buildproduct("fgps".to_string()).await;
     Ok(())
 }
 
 pub async fn buildproduct(name: String)  {
     let amalgam = Amalgam::new(vec!(name)).await;
-    println!("Sources: {:?}", amalgam.sources.len());
-    println!("Domains: {:?}", amalgam.domains.len());
+    // bare-bones signal that we got this far, for now.
+    println!("{}", amalgam);
 }
