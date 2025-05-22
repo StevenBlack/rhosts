5223cd274a32ae104d8c9c18defe87c9b29b1926
Steven Black <steveb@stevenblack.com>
2025-05-22 13:13:17 -0400

    Adding documentation.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,114 @@
-//! rhosts (rh) is a CLI for messing with hosts files
+//! This Rust module provides functionality for managing and processing hosts files
+//! with various options for comparison, sorting, and filtering. 
+//! 
+//! We use `clap` for command-line argument parsing and `anyhow` 
+//! for error handling.
+//!
+//! # Modules
+//!
+//! - **cmd**: Handles specific commands and their implementations.
+//! - **config**: Contains configuration management utilities and helpers.
+//! - **types**: Defines data structures and types.
+//! - **utils**: Includes utility functions for common tasks.
+//!
+//! # Main Structs and Enums
+//!
+//! ## `Arguments`
+//!
+//! The primary structure representing the parsed command-line arguments. This
+//! structure includes options for specifying hosts files, output preferences, and
+//! commands for execution.
+//!
+//! ### Fields
+//!
+//! - **mainhosts** (`String`): Specifies the main hosts file for comparison.
+//!   Defaults to `"base"`.
+//! - **comparehosts** (`Option<String>`): Specifies the hosts file to compare against the main hosts.
+//! - **isolate** (`Option<String>`): Specifies the hosts list to isolate and compare to the main hosts.
+//! - **iplocalhost** (`String`): Defines the IP address to use when listing hosts. Defaults to `"0.0.0.0"`.
+//! - **adddefaults** (`bool`): Adds default hosts to the top of the host lists.
+//! - **domains_sort** (`bool`): Enables domain sorting based on hierarchy.
+//! - **output** (`Option<String>`): Specifies an output file; otherwise, stdout is used.
+//! - **plain_output** (`bool`): Generates plain domain listings (domains only, without IP addresses).
+//! - **quiet** (`bool`): Enables quiet mode, displaying only the number of domains.
+//! - **stats** (`Option<bool>`): Displays statistics about the domains.
+//! - **intersection_list** (`bool`): Outputs the intersection of `mainhosts` and `comparehosts`.
+//! - **rootdomains** (`bool`): Outputs a count of root domains.
+//! - **tld** (`bool`): Outputs a tally of top-level domains (TLDs).
+//! - **limit** (`usize`): Limits the number of TLD/root domain listings. Defaults to 30; `0` for unlimited.
+//! - **skipheaders** (`bool`): Omits file comment headers in the output.
+//! - **showduplicates** (`bool`): Lists duplicate domains when reporting a hosts list.
+//! - **showinvalids** (`bool`): Lists invalid domains when reporting a hosts list.
+//! - **sysclipboard** (`bool`): Uses system clipboard contents as the compare hosts.
+//! - **uniquelist** (`bool`): Outputs unique domain names.
+//! - **verbose** (`bool`): Enables verbose output for development or debugging.
+//! - **command** (`Option<Commands>`): Specifies a subcommand to execute.
+//! - **skipcache** (`bool`): Prevents using cached data.
+//!
+//! ### Methods
+//!
+//! - **`Arguments::new`**: Generates a new `Arguments` instance with default or configured values.
+//!   - **Returns**: `Arguments`
+//!
+//! ## `Commands`
+//!
+//! Enum representing subcommands for the application.
+//!
+//! ### Variants
+//!
+//! - **Build**: Builds hosts files with an optional formula.
+//!   - **formula** (`Option<String>`): Specifies the formula to use.
+//! - **Cache**: Manages application cache with subcommands for actions.
+//!   - **cacheaction** (`Option<cmd::cache::CacheCommands>`): A subcommand for cache-specific actions.
+//! - **Init**: Initializes cache and templates.
+//! - **Info**: Displays additional application information.
+//!
+//! # Functions
+//!
+//! ## `show_info`
+//!
+//! Prints detailed application information such as version, description, author,
+//! license, homepage, and repository, as well as specific information from various
+//! application modules.
+//!
+//! ### Parameters
+//!
+//! - `args` (`Arguments`): The parsed command-line arguments.
+//!
+//! ### Returns
+//!
+//! - `Result<(), Error>`: Outputs `Ok(())` on success or an error if an issue occurs.
+//!
+//! ## `main`
+//!
+//! The entry point of the application that processes command-line inputs, initializes
+//! configurations, and executes the specified command or default behavior.
+//!
+//! ### Asynchronous Execution
+//!
+//! - Leverages `async_std::main` for asynchronous execution of certain tasks.
+//!
+//! ### Returns
+//!
+//! - `Result<(), Error>`: Outputs `Ok(())` on success or an error if initialization or execution fails.
+//!
+//! ### Behavior Based on Commands
+//!
+//! - Executes the given `Commands` variant or defaults to core command execution if no subcommand is provided.
+//!
+//! ## `test_default_command_line_arguments`
+//!
+//! A unit test to validate the default behavior and configuration of the `Arguments` struct.
+//!
+//! ### Assertions
+//!
+//! - Verifies default parsing and initialization of critical fields in `Arguments`:
+//!   - **`mainhosts`**: Fetched from the base key in shortcuts.
+//!   - **`comparehosts`**, **`iplocalhost`**, **`tld`**, and **`stats`**.
+//!
+//! ### Test Outcome
+//!
+//! - Ensures expected defaults for arguments and initializes properly when no command-line arguments are provided.
 
 extern crate clap;
 use anyhow::Error;
@@ -10,11 +120,117 @@ mod config;
 mod types;
 mod utils;
 
+/// Command-line arguments structure.
+///
+/// This struct is designed to parse and represent the input arguments.
+///
+/// # Fields
+///
+/// - `mainhosts`:
+///     The main hosts file acting as the basis for comparison. Defaults to "base".
+///     - Short Flag: `-m`
+///     - Long Flag: `--main`
+///
+/// - `comparehosts`:
+///     The hosts file to compare against the `mainhosts`.
+///     - Short Flag: `-c`
+///     - Long Flag: `--compare`
+///
+/// - `isolate`:
+///     A hosts file to isolate and compare against the `mainhosts`. This builds a temporary
+///     adjusted `mainhosts` without the isolated file, then compares the temporary adjusted 
+///     `mainhosts` with the isolated file.
+///     - Long Flag: `--isolate`
+///
+/// - `iplocalhost`:
+///     The IP address to associate with listed hosts. Defaults to `0.0.0.0`.
+///     - Long Flag: `--ip`
+///
+/// - `adddefaults`:
+///     Flag to include default hosts at the top of the hosts lists, if enabled.
+///     - Short Flag: `-d`
+///     - Long Flag: `--default_hosts`
+///
+/// - `domains_sort`:
+///     Flag to sort domains by order: domain, TLD, subdomains.
+///     - Short Flag: `-s`
+///     - Long Flag: `--sort`
+///
+/// - `output`:
+///     Specifies an output file. By default, output is sent to `stdout`.
+///     - Short Flag: `-o`
+///     - Long Flag: `--output`
+///
+/// - `plain_output`:
+///     Enables plain listing mode where only domains (without IP addresses) are listed.
+///     - Short Flag: `-p`
+///     - Long Flag: `--plain`
+///
+/// - `quiet`:
+///     Enables quiet output mode, only showing the count of domains.
+///     - Short Flag: `-q`
+///     - Long Flag: `--quiet`
+///
+/// - `stats`:
+///     Optional flag to display statistics about the domains.
+///     - Long Flag: `--stats`
+///
+/// - `intersection_list`:
+///     Prints the intersection of `mainhosts` and `comparehosts`.
+///     - Short Flag: `-i`
+///     - Long Flag: `--intersection`
+///
+/// - `rootdomains`:
+///     Lists root domains and their respective counts.
+///     - Short Flag: `-r`
+///     - Long Flag: `--rootdomains`
+///
+/// - `tld`:
+///     Displays a tally of top-level domains (TLDs) in the list.
+///     - Short Flag: `-t`
+///     - Long Flag: `--tld`
+///
+/// - `limit`:
+///     Sets a limit for listing TLDs and root domains, where `0` indicates no limit. Defaults to `30`.
+///     - Long Flag: `--limit`
+///
+/// - `skipheaders`:
+///     Omits file comment headers in the output.
+///     - Long Flag: `--skipheaders`
+///
+/// - `showduplicates`:
+///     Lists duplicate domains found in the hosts list.
+///     - Long Flag: `--showduplicates`
+///
+/// - `showinvalids`:
+///     Lists invalid domains detected in the hosts list.
+///     - Long Flag: `--invalid`
+///
+/// - `sysclipboard`:
+///     Uses the contents of the system clipboard as the `comparehosts` input.
+///     - Long Flag: `--clip`
+///
+/// - `uniquelist`:
+///     Lists unique domain names in the hosts list.
+///     - Short Flag: `-u`
+///     - Long Flag: `--unique`
+///
+/// - `verbose`:
+///     Enables verbose output, useful for debugging or detailed inspection.
+///     - Short Flag: `-v`
+///     - Long Flag: `--verbose`
+///
+/// - `command`:
+///     Specifies an optional subcommand to execute. Refer to the `Commands` enum for available
+///     subcommand options.
+///
+/// - `skipcache`:
+///     Disables caching to ensure fresh processing.
+///     - Long Flag: `--skipcache`
 #[derive(Debug, Default, Parser)]
 #[clap(author, version, about, long_about = None)]
 #[deny(missing_docs)]
 
-/// Tools to mess with hosts file
 #[derive(Clone)]
 pub struct Arguments {
     #[clap(
@@ -178,22 +394,54 @@ pub enum Commands {
     Info,
 }
 
-#[test]
-fn test_args() {
-    let d = Arguments::new();
-    assert_eq!(
-        d.mainhosts,
-        get_shortcuts()
-            .get("base")
-            .expect("The base key does not exist")
-            .to_owned()
-    );
-    assert_eq!(d.comparehosts, None);
-    assert_eq!(d.iplocalhost, "0.0.0.0".to_string());
-    assert_eq!(d.tld, false);
-    assert_eq!(d.stats, Some(true));
-}
-
+/**
+ * Displays detailed information about the current application, configuration, and related commands.
+ *
+ * This function provides an organized "info dump" that includes metadata about the application (such as version,
+ * description, author, license, homepage, and repository), as well as additional information from various components.
+ *
+ * # Arguments
+ *
+ * * `args` - A structure of type `Arguments` that holds any necessary command-line arguments or context.
+ *
+ * # Returns
+ *
+ * * `Ok(())` if the operation completes successfully.
+ * * `Err(Error)` if there is an issue during execution.
+ *
+ * # Metadata Displayed
+ *
+ * * Application version (from `CARGO_PKG_VERSION`).
+ * * Description of the application (from `CARGO_PKG_DESCRIPTION`).
+ * * Author(s) (from `CARGO_PKG_AUTHORS`).
+ * * License information (from `CARGO_PKG_LICENSE`).
+ * * Homepage URL (from `CARGO_PKG_HOMEPAGE`).
+ * * Repository URL (from `CARGO_PKG_REPOSITORY`).
+ *
+ * # Side Effects
+ *
+ * * Outputs formatted information to the standard output (`stdout`).
+ * * Delegates additional info retrieval and display to the following modules:
+ *   - `config` (via `config::info`).
+ *   - `cmd::cache` (via `cmd::cache::info`).
+ *   - `cmd::core` (via `cmd::core::info`).
+ *
+ * # Formatting
+ *
+ * The output includes a centered title ("info dump") bounded by dashes, followed by sections of metadata
+ * and component-specific information, all separated by blank lines for better readability.
+ *
+ * # Example
+ *
+ * ```
+ * use your_crate::Arguments;
+ *
+ * let args = Arguments::new(); // Example initialization of `Arguments`.
+ * if let Err(e) = show_info(args) {
+ *     eprintln!("Error displaying information: {}", e);
+ * }
+ * ```
+ */
 fn show_info(args:Arguments) -> Result<(), Error> {
     println!("");
     println!("{}",format!("{:-^1$}", " info dump ", 40));
@@ -237,4 +485,37 @@ async fn main() -> Result<(), Error> {
         std::process::exit(101);
     }
     Ok(())
+}
+
+
+#[test]
+fn test_default_command_line_arguments() {
+    let arguments = Arguments::new();
+    assert_eq!(
+        arguments.mainhosts,
+        get_shortcuts()
+            .get("base")
+            .expect("The base key does not exist")
+            .to_owned(),
+        "Expected mainhosts to be fetched by the base key"
+    );
+    assert_eq!(
+        arguments.comparehosts
+        , None,
+        "Expected the comparehosts argument to be None"
+    );
+    assert_eq!(
+        arguments.iplocalhost
+        , "0.0.0.0".to_string(),
+        "Expected the iplocalhost argument to be 0.0.0.0"
+    );
+    assert_eq!(
+        arguments.tld
+        , false,
+        "Expected the tld argument to be false"
+    );
+    assert_eq!(
+        arguments.stats, Some(true),
+        "Expected the stats argument to be Some(true)"
+    );
 }
